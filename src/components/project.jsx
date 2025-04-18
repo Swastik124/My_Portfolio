87bf72023699b1de7df6c9d80082a7d07f03ac1b
Swastik124 <swastikmohanty124@gmail.com>
2025-04-18 18:34:04 +0530

    Added Skills Section

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -12,23 +12,23 @@ export default function Project() {
         <div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5">
             {/* Project 1 */}
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse">
-              <div className="md:flex">
-                <div className="p-8">
-                  <div className="uppercase tracking-wide text-sm to-indigo-500 font-semibold">
-                    Advanced Keylogger
+                  <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse">
+                    <div className="md:flex">
+                    <div className="p-8">
+                      <div className="uppercase tracking-wide text-sm to-indigo-500 font-semibold">
+                      Advanced Keylogger
+                      </div>
+                      <a href="https://github.com/Swastik124/Advanced_Keylogger.git" target='_blank' rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><u>View Project</u></a>
+                      <p className="mt-2 text-slate-500">
+                      This project is a Python-based keylogger that collects keystrokes, clipboard data, system information, microphone audio, and screenshots. The collected data is encrypted using the cryptography library and sent securely via email for analysis. It is designed for educational purposes and cybersecurity research.
+                      </p>
+                    </div>
+                    <div className='md:shrink-0 p-5 flex items-center'>
+                      <img src={Keylogger} alt="Project1" width={150} height={150}/>
+                    </div>
+                    </div>
                   </div>
-                  <a href="https://github.com/Swastik124/Advanced_Keylogger.git" target='_blank' rel="noopener noreferrer">Keylogger</a>
-                  <p className="mt-2 text-slate-500">
-                    This project is a Python-based keylogger that collects keystrokes, clipboard data, system information, microphone audio, and screenshots. The collected data is encrypted using the cryptography library and sent securely via email for analysis. It is designed for educational purposes and cybersecurity research.
-                  </p>
-                </div>
-                <div className='md:shrink-0 p-5 flex items-center'>
-                  <img src={Keylogger} alt="Project1" width={150} height={150}/>
-                </div>
-              </div>
-            </div>
-            {/* Project 2 */}
+                  {/* Project 2 */}
             <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse">
               <div className="md:flex">
                 <div className='md:shrink-0 p-5 flex items-center'>
@@ -38,7 +38,7 @@ export default function Project() {
                   <div className="uppercase tracking-wide text-sm to-indigo-500 font-semibold">
                     Smart Cafeteria System
                   </div>
-                  <a href="https://github.com/Swastik124/speech-to-text-transcription.git" target='_blank' rel="noopener noreferrer">Speech to Text Transcription</a>
+                  <a href="https://github.com/Swastik124/speech-to-text-transcription.git" target='_blank' rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><u>View Project</u></a>
                   <p className="mt-2 text-slate-500">
                     A smart cafeteria ordering system using ESP32, Bluetooth, and WiFi that allows users to place orders via Bluetooth commands and view real-time updates on a web interface. Orders are logged on an SD card, and the menu is dynamically loaded from a JSON file. 🚀
                   </p>
@@ -52,7 +52,7 @@ export default function Project() {
                   <div className="uppercase tracking-wide text-sm to-indigo-500 font-semibold">
                     TraveloCity Clone
                   </div>
-                  <a href="https://github.com/Swastik124/TraveloCity-Clone.git" target='_blank' rel="noopener noreferrer">TraveloCity</a>
+                  <a href="https://github.com/Swastik124/TraveloCity-Clone.git" target='_blank' rel="noopener noreferrer"className="text-blue-500 hover:text-blue-700"><u>View Project</u></a>
                   <p className="mt-2 text-slate-500">
                     This project employs technologies such as HTML, CSS, JavaScript, and relevant frameworks (React) to mimic real-world functionalities. It serves as an educational resource and a portfolio piece to showcase the ability to recreate complex, real-world web applications.
                   </p>
